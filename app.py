38962e696a5ea6162960178f84200de98695a9eb
AI_Programming <118508502+IA-Programming@users.noreply.github.com>
2023-09-28 03:39:31 +0000

    Pending changes exported from your codespace

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ if 'OpenAI_api_key' not in st.session_state:
     st.session_state.OpenAI_api_key = ''
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+    # interpreter.load(st.session_state.messages)
 
 st.set_page_config(
    page_title="Open-Interpreter Gpt App",
@@ -37,7 +38,7 @@ with st.sidebar:
             st.session_state.widget = ''
             # st.session_state.OpenAI_api_key = ''
             st.info("Please add your OpenAI API key Correctly to continue.")
-            # st.stop()
+            
 
     st.text_input('OpenAI API Key', key='widget', on_change=submit, type="password")
 
@@ -46,10 +47,9 @@ for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
 if prompt := st.chat_input(placeholder="Write here your message", disabled=not st.session_state.OpenAI_api_key):
-
-    openai.api_key = st.session_state.OpenAI_api_key
+    interpreter.auto_run = True
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    response = interpreter.chat(prompt)
+    response = interpreter.chat(prompt, return_messages=True)
     st.session_state.messages.append(response)
     st.chat_message("assistant").write(response)
